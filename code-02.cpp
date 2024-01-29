8df9bbebbcd2664d217525e2b37c27961828aaf0
sarutnote <sarut.n@ku.th>
2024-01-29 14:33:47 +0700

    update code-02.cpp on branch main

--- code-02.cpp
+++ code-02.cpp
@@ -4,6 +4,6 @@ using namespace std;
 int main(){
     // cout : console output ไม่อ่านเค้า อ่าน ซีเอ้า
     // endl : end line '\n'
-    cout << "Hello Git" << endl;
+    cout << "Love C++" << endl;
 
 }
